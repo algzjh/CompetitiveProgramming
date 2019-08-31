e519f439613c548a1d48b8ff80567ea0966a02e7
algzjh <jiehuizhou1997@gmail.com>
2019-08-31 19:29:40 +0800

    math, 900

--- Codeforces/1213A.cpp
+++ Codeforces/1213A.cpp
@@ -5,29 +5,22 @@
 using namespace std;
 typedef long long LL;
 typedef pair<int, int> PII;
-const int MAXN = 2e3 + 5;
-const int MAXM = 1e4 + 5;
+const int MAXN = 2e5 + 5;
+const int MAXM = 2e5 + 5;
 const int MOD = 998244353;
-const LL INF = 1e18;
-int x[MAXN];
+const int INF = 0x3f3f3f3f;
+int fa[MAXN], sz[MAXN];
+LL a[MAXN];
  
  
 int main(){
     int n;
     scanf("%d", &n);
+    int odd = 0, x;
     for(int i = 1; i <= n; ++i){
-        scanf("%d", &x[i]);
+        scanf("%d", &x);
+        if(x & 1) ++odd;
     }
-    sort(x + 1, x + n + 1);
-    LL ans = INF, tmp;
-    for(int i = 1; i <= n; ++i){
-        tmp = 0;
-        for(int j = 1; j <= n; ++j){
-            if(i == j) continue;
-            if(abs(x[i] - x[j]) & 1)  ++tmp;
-        }
-        ans = min(ans, tmp);
-    }
-    printf("%lld\n", ans);
+    printf("%d\n", min(odd, n - odd));
     return 0;
 }
