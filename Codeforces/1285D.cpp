98ac0e981ad7073d7334596dfc885f894dadebf8
algzjh <jiehuizhou1997@gmail.com>
2020-01-12 23:50:54 +0800

    bitmasks, dfs and similar, dp, greedy

---
+++ Codeforces/1285D.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+#define fi first
+#define se second
+#define mk make_pair
+using namespace std;
+const int MAXN = static_cast<const int>(1e5 + 5);
+const int MAXM = static_cast<const int>(2e5 + 5);
+const int MOD = static_cast<const int>(1e9 + 7);
+const int INF = 0x3f3f3f3f;
+typedef long long LL;
+typedef pair<int, int> PII;
+int n;
+vector<int> a;
+
+int solve(vector <int> &v, int bit){
+    if(bit < 0)  return 0;
+    vector <int> t0, t1;
+    for(auto &i : v){
+        if(((i>>bit)&1) == 0)  t0.emplace_back(i);
+        else t1.emplace_back(i);
+    }
+    if(t0.size() == 0)  return solve(t1, bit - 1);
+    if(t1.size() == 0)  return solve(t0, bit - 1);
+    return min(solve(t0, bit - 1), solve(t1, bit - 1)) + (1 << bit);
+}
+
+int main(){
+    scanf("%d", &n);
+    int x;
+    for(int i = 1; i <= n; ++i){
+        scanf("%d", &x);
+        a.emplace_back(x);
+    }
+    printf("%d\n", solve(a, 30));
+    return 0;
+}
+/*
+3 4 6 7
+ */
