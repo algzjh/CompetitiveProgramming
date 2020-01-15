c06d6a287c44af8a2b2fa5bb7dfdf763db329a18
algzjh <jiehuizhou1997@gmail.com>
2020-01-15 10:19:37 +0800

    greedy, sortings, 1600

---
+++ Codeforces/1282B2-sol2.cpp
@@ -0,0 +1,65 @@
+/**
+ * 先从小到大排序
+ * 最终买东西的段一定是连续的，否则可以向左平移，使得花费更少
+ * 另外一定只有小于 k 个没有使用优惠券购买的商品，否则干嘛不使用呢？
+ * 另外，这些没有使用优惠券购买的商品一定是最便宜的那些，这样花费更少~
+ * 故只需枚举前面 k 个没有使用券的商品是哪些，然后 k 步跳就行了
+ */
+ 
+#include<bits/stdc++.h>
+#define fi first
+#define se second
+#define mk make_pair
+using namespace std;
+const int MAXN = static_cast<const int>(2e3 + 5);
+const int MAXM = static_cast<const int>(2e5 + 5);
+const int MOD = static_cast<const int>(1e9 + 7);
+const int INF = 0x3f3f3f3f;
+typedef long long LL;
+typedef pair<int, int> PII;
+int n, p[MAXN], c[MAXN], a[MAXN];
+vector<int> E[MAXN];
+ 
+int main() {
+    int cntTest;
+    cin >> cntTest;
+    for (int test = 0; test < cntTest; test++) {
+        int n, p, k;
+        cin >> n >> p >> k;
+        int pref = 0;
+        int ans = 0;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        sort(a.begin(), a.end());
+        for (int i = 0; i <= k; i++) {
+            int sum = pref;
+            if (sum > p) break;
+            int cnt = i;
+            for (int j = i + k - 1; j < n; j += k) {
+                if (sum + a[j] <= p) {
+                    cnt += k;
+                    sum += a[j];
+                } else {
+                    break;
+                }
+            }
+            pref += a[i];
+            ans = max(ans, cnt);
+        }
+        cout << ans << "\n";
+    }
+}
+/*
+10
+8 6 3
+1 1 1 2 3 8 9 10
+ 
+ 
+10
+8 9 3
+1 1 2 2 3 4 5 7
+ 
+ 
+ */
