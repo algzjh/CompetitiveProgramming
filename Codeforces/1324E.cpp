2250aee442544acd3128c0a4ea38dc3a2305080c
algzjh <jiehuizhou1997@gmail.com>
2020-03-16 20:02:51 +0800

    dp, implementation, 1700

--- Codeforces/1324E.cpp
+++ Codeforces/1324E.cpp
@@ -9,36 +9,36 @@ const int INF = 0x3f3f3f3f;
 typedef long long LL;
 typedef pair<int, int> PII;
 typedef pair<LL, LL> PLL;
-int n, h, l, r, x, sum;
-int dp[MAXN][MAXN];
- 
- 
+int n, h, l, r;
+int dp[2][MAXN];
+
+
 void init(){
     memset(dp, -1, sizeof(dp));
     dp[0][0] = 0;
-    sum = 0;
 }
- 
- 
+
+
 int main(){
     init();
     scanf("%d%d%d%d", &n, &h, &l, &r);
+    int x, tim, cur = 0, sum = 0;
     for(int i = 1; i <= n; ++i){
         scanf("%d", &x);
         sum += x;
-        int now;
+        cur ^= 1;
         for(int j = 0; j < i; ++j){
-            if(dp[i-1][j] >= 0){
-                now = (sum - j) % h;
-                dp[i][j] = max(dp[i][j], dp[i-1][j] + (l <= now and now <= r));
-                now = (now - 1 + h) % h;
-                dp[i][j + 1] = max(dp[i][j + 1], dp[i-1][j] + (l <= now and now <= r));
+            if(dp[cur^1][j] >= 0){
+                for(int k = j; k <= j + 1; ++k){
+                    tim = (sum - k + h) % h;
+                    dp[cur][k] = max(dp[cur][k], dp[cur^1][j] + (l <= tim and tim <= r));
+                }
             }
         }
     }
     int ans = 0;
     for(int j = 0; j <= n; ++j){
-        ans = max(ans, dp[n][j]);
+        ans = max(ans, dp[cur][j]);
     }
     printf("%d\n", ans);
     return 0;
