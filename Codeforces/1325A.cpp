a815b96a6e7e3387bae2233f81fe621c17bef987
algzjh <jiehuizhou1997@gmail.com>
2020-03-17 17:42:23 +0800

    constructive algorithms, number theory

---
+++ Codeforces/1325A.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define fi first
+#define se second
+#define mk make_pair
+using namespace std;
+const int MAXN = 2e3 + 5;
+const int MOD = 1e9 + 7;
+const int INF = 0x3f3f3f3f;
+typedef long long LL;
+typedef pair<int, int> PII;
+typedef pair<LL, LL> PLL;
+
+
+int main(){
+    int _;
+    scanf("%d", &_);
+    while(_--){
+        int x;
+        scanf("%d", &x);
+        printf("%d %d\n", 1, x - 1);
+    }
+    return 0;
+}
