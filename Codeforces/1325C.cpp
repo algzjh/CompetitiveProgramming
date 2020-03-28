ee72764773b5c91ad2491015666be2a970ef6289
algzjh <jiehuizhou1997@gmail.com>
2020-03-28 23:49:12 +0800

    constructive algorithms, greedy, trees, 1500

---
+++ Codeforces/1325C.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int MAXN = 1e5 + 5;
+int ans[MAXN], n;
+
+struct Edge{
+    int to, id;
+};
+vector<Edge> G[MAXN];
+
+
+int main() {
+    scanf("%d", &n);
+    int u, v;
+    for(int i = 1; i <= n - 1; ++i){
+        scanf("%d%d", &u, &v);
+        G[u].emplace_back(Edge{v, i});
+        G[v].emplace_back(Edge{u, i});
+        ans[i] = -1;
+    }
+    for(int i = 1; i <= n; ++i){
+        if(G[i].size() >= 3){
+            for(int j = 0; j < 3; ++j){
+                int id = G[i][j].id;
+                ans[id] = j;
+            }
+            break;
+        }
+    }
+    int now = 3;
+    for(int i = 1; i <= n - 1; ++i){
+        if(ans[i] == -1){
+            ans[i] = now;
+            ++now;
+        }
+    }
+    for(int i = 1; i <= n - 1; ++i){
+        printf("%d\n", ans[i]);
+    }
+    return 0;
+}
+/*
+7
+6 4
+3 7
+3 5
+1 3
+4 2
+7 4
+ */
