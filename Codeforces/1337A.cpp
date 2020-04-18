ac2494c4c4d040e126e32d2a59ab862cb8a6351a
algzjh <jiehuizhou1997@gmail.com>
2020-04-18 23:50:52 +0800

    constructive algorithms, math, 1000

---
+++ Codeforces/1337A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+const int MAXN = 2e3 + 5;
+int p[MAXN], dp[MAXN][10][25];
+ 
+ 
+int main() {
+    int _;
+    scanf("%d", &_);
+    while(_--){
+        int a, b, c, d;
+        scanf("%d%d%d%d", &a, &b, &c, &d);
+        printf("%d %d %d\n", a, c, c);
+    }
+    return 0;
+}
