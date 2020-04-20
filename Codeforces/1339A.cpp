21a2c51573067f4c2a2e2cc2617e8d7adb91a4a8
algzjh <jiehuizhou1997@gmail.com>
2020-04-20 23:26:59 +0800

    implementation, math, 1000

---
+++ Codeforces/1339A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long LL;
+
+
+int main() {
+    int _;
+    scanf("%d", &_);
+    while(_--){
+        int n;
+        scanf("%d", &n);
+        printf("%d\n", n);
+    }
+    return 0;
+}
+/*
+1: 1
+2: 2:
+3:
+*/
