115b4e06ff35325d0c1e4e32324c242b818b18cc
zhoujiehui <jiehuizhou1997@gmail.com>
2020-05-14 21:29:27 +0800

    data structure greedy

--- Hunan Multi-University Competition/2020/Contest4/D.java
+++ Hunan Multi-University Competition/2020/Contest4/D.java
@@ -11,93 +11,90 @@ public class Main {
             l = _l;
             id = _id;
         }
+
     }
 
-    static class Servercomparator implements Comparator<Server> {
+    static class ServerComparator implements Comparator<Server> {
         @Override
-        public int compare(Server server, Server t1) {
-            if (server.a == t1.a)  return server.l > t1.l ? -1 : 1;
-            return server.a > t1.a ? -1 : 1;
+        public int compare(Server lhs, Server rhs) {
+            if(lhs.a == rhs.a){
+                if(lhs.l > rhs.l)  return -1;
+                else if(lhs.l < rhs.l)  return 1;
+                return 0;
+            }
+            return  lhs.a > rhs.a ? -1 : 1;
         }
     }
 
     private static void init(){
         servers.clear();
-        zero_list.clear();
-        one_list.clear();
+        zero_stack.clear();
         ans_list.clear();
         low_voltage_cnt = 0;
     }
 
-    static final int MAXN = (int) (2e3 + 5);
     static ArrayList<Server> servers = new ArrayList<>();
-    static ArrayList<Server> zero_list = new ArrayList<>();
-    static ArrayList<Server> one_list = new ArrayList<>();
-    static ArrayList<Server> ans_list = new ArrayList<>();
+    static Stack<Server> zero_stack = new Stack<>();
+    static ArrayList<Integer> ans_list = new ArrayList<>();
     static int low_voltage_cnt;
 
     public static void main(String[] args) {
-        ArrayList<Integer> temp = new ArrayList<>();
-        Collections.reverse(temp);
-
         Scanner sc = new Scanner(System.in);
-        int n;
-        long m;
-        n = sc.nextInt();
+        long n, m;
+        n = sc.nextLong();
         m = sc.nextLong();
         init();
+
         for (int i = 1; i <= n; ++i) {
             Server server = new Server(sc.nextLong(), sc.nextInt(), i);
             servers.add(server);
         }
-        Collections.sort(servers, new Servercomparator());
+        Collections.sort(servers, new ServerComparator());
+
         long sum = 0;
 
         for (Server s : servers) {
-//            System.out.println(s.a + " " + s.l);
             if (sum < m) {
                 sum += s.a;
                 if(s.l == 0) {
-                    zero_list.add(s);
+                    zero_stack.push(s);
                 }else{
-                    ans_list.add(s);
-                    low_voltage_cnt += 1;
+                    ans_list.add(s.id);
+                    ++low_voltage_cnt;
                 }
             }else{
+                if(zero_stack.empty())  break;
                 if(s.l == 1){
-                    one_list.add(s);
+                    if(sum - zero_stack.peek().a + s.a >= m){
+                        sum = sum - zero_stack.peek().a + s.a;
+                        ans_list.add(s.id);
+                        zero_stack.pop();
+                        ++low_voltage_cnt;
+                    }
                 }
             }
         }
-        long margin = sum - m;
-        Collections.reverse(zero_list);
-        int p = 0;
-        for(int i = 0; i < zero_list.size(); ++i){
-            if(p < one_list.size()){
-                long reduce = zero_list.get(i).a - one_list.get(p).a;
-                if(margin >= reduce){
-                    margin -= reduce;
-                    ans_list.add(one_list.get(p));
-                    ++low_voltage_cnt;
-                    ++p;
-                }else{
-                    ans_list.add(zero_list.get(i));
-                }
-            }else{
-                ans_list.add(zero_list.get(i));
-            }
+
+        while(!zero_stack.empty()){
+            ans_list.add(zero_stack.peek().id);
+            zero_stack.pop();
         }
 
         System.out.println(ans_list.size() + " " + low_voltage_cnt);
         for(int i = 0; i < ans_list.size(); ++i){
-            System.out.print(String.format("%d%c", ans_list.get(i).id, i == ans_list.size() - 1 ? '\n' : ' '));
+            System.out.print(ans_list.get(i));
+            if(i == ans_list.size() - 1){
+                System.out.print("\n");
+            }else{
+                System.out.print(" ");
+            }
         }
     }
 }
 /*
 4 10
 3 1
-7 0
-5 1
 4 1
+2 1
+1 1
  */
