2686cdb4d1eccb9bb42c734403f881819f90d390
zhoujiehui <jiehuizhou1997@gmail.com>
2020-05-20 10:42:55 +0800

    greedy implementation

--- Hunan Multi-University Competition/2020/Contest5/E.java
+++ Hunan Multi-University Competition/2020/Contest5/E.java
@@ -11,58 +11,24 @@ public class Main {
     }
 
     public static void main(String[] args) throws IOException {
-        File file = new File("data/alex.in");
-//        File file = new File("easy.in");
-//        BufferedWriter writer = new BufferedWriter(new FileWriter("easy.out"));
-        BufferedWriter writer = new BufferedWriter(new FileWriter("data/alex.out"));
+//        File file = new File("data/alex.in");
+        File file = new File("easy.in");
+        BufferedWriter writer = new BufferedWriter(new FileWriter("easy.out"));
+//        BufferedWriter writer = new BufferedWriter(new FileWriter("data/alex.out"));
         Scanner sc = new Scanner(file);
 
         init();
         expr = sc.nextLine();
-        boolean flag = false, jump = false;
+        boolean flag = false;
 
         for(int i = 0; i < expr.length(); ++i){
+            if(expr.charAt(i) == '+' || expr.charAt(i) == '-')  flag = false;
+            if(flag)  ans += '+';
+            if(expr.charAt(i) != '0')  flag = false;
+            if(i > 0 && expr.charAt(i-1) == '-')  flag = true;
             ans += expr.charAt(i);
-            if(expr.charAt(i) == '-'){
-                flag = true;
-            }else if(expr.charAt(i) == '+'){
-                flag = false;
-            }else if(flag){
-                if(i + 1 < expr.length()){
-                    if(expr.charAt(i + 1) == '+' || expr.charAt(i + 1) == '-'){
-                        flag = false;
-                    }else if(expr.charAt(i + 1) == '0'){
-                        while(i + 2 < expr.length()){
-                            ++i;
-                            ans += expr.charAt(i);
-                        }
-
-                        while(i + 2 < )
-
-                        while(i + 2 < expr.length() || )
-                    }else{
-                        ans += "+";
-                        flag = false;
-                    }
-                }
-
-                if(i + 1 < expr.length()){
-
-
-                    if(expr.charAt(i + 1) != '-' && expr.charAt(i + 1) != '+' && expr.charAt(i + 1) != '0'){
-                        ans += "+";
-                        flag = false;
-                    }
-                }else{
-                    ans += "+";
-                    flag = false;
-                }
-            }
         }
         writer.write(ans);
         writer.close();
     }
 }
-/*
-
-*/
