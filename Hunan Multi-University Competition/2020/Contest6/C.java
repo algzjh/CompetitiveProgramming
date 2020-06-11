c84a67893e896dd80e0f8285ec7b1ac561f8c247
zhoujiehui <jiehuizhou1997@gmail.com>
2020-06-12 01:18:10 +0800

    dp binary checkbit

--- Hunan Multi-University Competition/2020/Contest6/C.java
+++ Hunan Multi-University Competition/2020/Contest6/C.java
@@ -1,36 +1,35 @@
  import java.io.*;
- import java.lang.reflect.Array;
  import java.util.*;
 
  public class Main {
     private static final int MAXN = (int) (4e4 + 5);
-    private static int MOD = (int) (1e9 + 7);
+    private static final int MOD = (int) (1e9 + 7);
     private static int n, k;
     private static long[][] dp = new long[MAXN][16];
     private static long[] selectCardsRes = new long[MAXN];
-    private static Card[] cards = new Card[MAXN];
+    private static Card[] cards = new Card[16];
 
     static class Card{
-        Card(int _w, int _h, int _q){
+        long w, h, q;
+        Card(long _w, long _h, long _q){
             w = _w;
             h = _h;
             q = _q;
         }
-        int w, h, q;
     }
 
-    private static void init(){
+    static void init(){
         for(int i = 1; i < (1<<n); ++i){
             long maxW = 0, maxH = 0;
             for(int j = 0; j < n; ++j){
-                if(((1<<j)&i) == 1){
+                if((i & (1<<j)) != 0){
                     maxW = Math.max(maxW, cards[j].w);
                     maxH = Math.max(maxH, cards[j].h);
                 }
             }
             long res = 0;
             for(int j = 0; j < n; ++j){
-                if(((1<<j)&i) == 1){
+                if((i & (1<<j)) != 0){
                     res += (cards[j].q * (maxW * maxH - cards[j].w * cards[j].h));
                 }
             }
@@ -43,7 +42,7 @@
         for(int i = 1; i < k; ++i){
             for(int j = 1; j < (1<<n); ++j){
                 dp[j][i] = dp[j][i-1];
-                for(int k = j; k > 0; k = (k - 1)&j){
+                for(int k = j; k > 0; k = ((k - 1)&j)){
                     dp[j][i] = Math.min(dp[j][i], dp[k][i - 1] + selectCardsRes[k^j]);
                 }
             }
@@ -56,7 +55,7 @@
         while(sc.hasNext()){
             n = sc.nextInt();
             k = sc.nextInt();
-            int w, h, q;
+            long w, h, q;
             for(int i = 0; i < n; ++i){
                 w = sc.nextInt();
                 h = sc.nextInt();
