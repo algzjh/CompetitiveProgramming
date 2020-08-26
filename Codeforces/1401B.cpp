634c727f4457e88be6cce62a4703ad78fa4ad174
algzjh <jiehuizhou1997@gmail.com>
2020-08-27 01:32:31 +0800

    greedy, math, constructive algorithms, 1100

--- Codeforces/1401B.cpp
+++ Codeforces/1401B.cpp
@@ -1,7 +1,7 @@
 /*
  * Author: algzjh
- * Date: 22/8/2020
- * Comment: math, 900
+ * Date: 27/8/2020
+ * Comment: greedy, math, constructive algorithms, 1100
  */
 #include <bits/stdc++.h>
 #define mk make_pair
@@ -14,7 +14,7 @@ const int MAXN = 1e5 + 5;
 const int MAXM = 1e6 + 5;
 const LL INF = 1e18;
 const int MOD = 998244353;
-int c[2][3];
+LL c[2][3];
 
 int main(){
     int _;
@@ -22,16 +22,35 @@ int main(){
     while(_--){
         for(int i = 0; i < 2; ++i){
             for(int j = 0; j < 3; ++j){
-                scanf("%d", &c[i][j]);
+                scanf("%lld", &c[i][j]);
             }
         }
-        int ans = 2*min(c[0][2], c[1][1]);
-        if(c[0][1] > c[1][0]){
-            ans -= 2*min(c[0][1] - c[1][0], c[1][2]);
-        }
-        printf("%d\n", ans);
+        LL ans = 0;
+        LL m = min(c[0][2], c[1][1]);
+        c[0][2] -= m;
+        c[1][1] -= m;
+        ans += 2 * m;
+
+        m = min(c[0][1], c[1][0]);
+        c[0][1] -= m;
+        c[1][0] -= m;
+
+        m = min(c[0][0], c[1][2]);
+        c[0][0] -= m;
+        c[1][2] -= m;
+
+        ans -= 2 * min(c[0][1], c[1][2]);
+
+        printf("%lld\n", ans);
     }
     return 0;
 }
 /*
+3
+2 3 2
+3 3 1
+4 0 1
+2 3 0
+0 0 1
+0 0 1
 */
