da8ec9c46f6b617b3b98233d133e464f227fc055
algzjh <jiehuizhou1997@gmail.com>
2020-09-21 23:34:19 +0800

    greedy, math, 1100

---
+++ Codeforces/1409B.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<cstdio>
+#include<algorithm>
+#include<cstring>
+using namespace std;
+const int MAXN=1e6+5;
+typedef long long LL;
+
+int main(){
+    int _;
+    scanf("%d", &_);
+    while(_--){
+        LL a, b, x, y, n;
+        scanf("%lld%lld%lld%lld%lld", &a, &b, &x, &y, &n);
+        if(a - x + b - y <= n){
+            printf("%lld\n", x * y);
+        }else{
+            LL p1 = max(n + y - b, 0LL);
+            LL p2 = min(a - x, n);
+            LL ans = min(a * b - a * n + (a - b + n) * p1 - p1 * p1,
+                         a * b - a * n + (a - b + n) * p2 - p2 * p2);
+            printf("%lld\n", ans);
+        }
+    }
+    return 0;
+}
+/*
+
+*/
